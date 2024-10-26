a8682727e378cc472f53ea62248120dc7e7d17a5
Samuel Mast <samuelmst@gmail.com>
2024-10-26 14:33:19 -0500

    Basic Card Movement

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,26 +1,74 @@
-extends Control
+extends Node2D
 
-@onready var card_texture = $CardTexture
-var deck = ["C1","C2","C3","C4","C5","C6","C7","C8","C9","C10","C11","C12","C13"]
-var card = ""
-#var card_texture = ""
-var faceup = false
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	card = deck[0]
+# Ace is 1, King is 13
+@export var value = 1
+# Club is 0, Spade is 2, Heart is 1, Diamond is 3
+@export var suite = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
+var draggable = false
+var current_drag = false
+var can_drop = false
+var body_ref
+var offset : Vector2
+var initialPos : Vector2
+@onready var area_2d: Area2D = $Area2D
+@onready var card: Node2D = $"."
 
-func _gui_input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.pressed == false:
-			flip()
-			
-func flip():
-	if faceup:
-		card_texture.texture = load("res://assets/sprites/cards/Backs/back_0.png")
-		faceup =! faceup
-	else:
-		card_texture.texture = load("res://assets/sprites/cards/" + card + ".png")
-		faceup =! faceup
+# Constructor
+#func _init(s,v) -> void:
+#	value = v
+#	suite = s
+
+
+func _process(delta: float) -> void:
+	if !draggable:
+		return
+	
+	if Input.is_action_just_pressed("click"):
+		current_drag = true
+		initialPos = global_position
+		offset = get_global_mouse_position() - global_position
+		Global.is_dragging = true
+	if Input.is_action_pressed("click"):
+		global_position = get_global_mouse_position() - offset
+	elif Input.is_action_just_released("click"):
+		Global.is_dragging = false
+		current_drag = false
+		var tween = get_tree().create_tween()
+		if can_drop:
+			card.z_index = body_ref.z_index + 1
+			tween.tween_property(self, "position", body_ref.global_position - Vector2(0,-10), 0.2).set_ease(Tween.EASE_OUT)
+		else:
+			tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
+
+
+
+func _on_area_2d_mouse_entered() -> void:
+	if !Global.is_dragging:
+		draggable = true
+
+func _on_area_2d_mouse_exited() -> void:
+	if !Global.is_dragging:
+		draggable = false
+
+#func _on_area_2d_area_entered(area: Area2D) -> void:
+#	if current_drag and area.is_in_group('droppable'):
+#		can_drop = true
+#		area_ref = area
+
+
+#func _on_area_2d_area_exited(area: Area2D) -> void:
+#	if current_drag and area.is_in_group('droppable'):
+#		can_drop = false
+		
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if current_drag and body.is_in_group('droppable'):
+		can_drop = true
+		body_ref = body
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if current_drag and body.is_in_group('droppable'):
+		can_drop = false
